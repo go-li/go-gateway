cdf17b6e80c9df188e321c5e463c973bf9b14b08
Goli Admin <charraster@gmail.com>
2016-06-03 16:45:22 +0200

    Now we have also the security

--- dispatch.go
+++ dispatch.go
@@ -1,37 +1,51 @@
 package main
 
 import (
-"os/exec"
+	"os/exec"
+	"time"
 )
+
+const timeout = 10
 
 type Dispatcher [24]byte
 
 func (d Dispatcher) Read(p []byte) (n int, err error) {
-var buf = []byte("open upload.trepstar.com\nuser public Phebru3U\n"+
-"put /tmp/________________________.js ________________________.js\n\000")
-copy(p,buf)
-copy(p[55:55+24],d[:])
-copy(p[55+24+4:55+24+4+24],d[:])
-return len(buf),nil
+	var buf = []byte("open upload.trepstar.com\nuser public Phebru3U\n" +
+		"put /tmp/________________________.js ________________________.js\n\000")
+	copy(p, buf)
+	copy(p[55:55+24], d[:])
+	copy(p[55+24+4:55+24+4+24], d[:])
+	return len(buf), nil
 }
- func (Dispatcher)       Close() error {
-	return nil;
+func (Dispatcher) Close() error {
+	return nil
 }
 
-func dispatch(s string) {
+func dispatch(s string) error {
 
 	// prepare dispatcher
 	var d [24]byte
-	copy(d[0:24],[]byte(s))
+	copy(d[0:24], []byte(s))
 
-	print("DISPATCHING "+s+"...\n");
-
+	print("DISPATCHING " + s + "...\n")
 
 	cmd := exec.Command("ftp", "-n")
 	cmd.Stdin = Dispatcher(d)
 	cmd.Start()
 
+	done := make(chan error, 1)
+	go func() {
+		done <- cmd.Wait()
+	}()
 
+	select {
+	case <-time.After(timeout * time.Second):
+		if err := cmd.Process.Kill(); err != nil {
+			return err
+		}
+		print("process killed as timeout reached\n")
+	case err := <-done:
+		return err
+	}
+	return nil
 }
-
-
--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"io/ioutil"
 	"bytes"
-        "encoding/json"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
-    "strconv"
+	"strconv"
 )
 
 const gateport = "8333"
-
 
 // UrlEncoded encodes a string like Javascript's encodeURIComponent()
 func UrlEncoded(str string) (string, error) {
@@ -23,78 +22,97 @@ func UrlEncoded(str string) (string, error) {
 }
 
 type outside struct {
-	In inside      `json:"0"`
+	In inside `json:"0"`
 }
 
 type inside struct {
-    Id   string      `json:"_id"`
-    Version int `json:"version"`
-    Body string `json:"body"`
-    Compiler string `json:"compiler"`
+	Id       string `json:"_id"`
+	Version  int    `json:"version"`
+	Body     string `json:"body"`
+	Compiler int    `json:"compiler"`
+}
+
+func perr(e error) {
+	fmt.Println(e)
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
-// First, decode the json
+	// First, decode the json
 
-    decoder := json.NewDecoder(req.Body)
-    var t outside
-    err1 := decoder.Decode(&t)
-	if (err1 != nil) {print("ERR1");return;}
+	decoder := json.NewDecoder(req.Body)
+	var t outside
+	err1 := decoder.Decode(&t)
+	if err1 != nil {
+		print("ERR1")
+		return
+	}
 
+	data := url.Values{}
+	data.Set("version", "2")
+	data.Add("body", t.In.Body)
 
-    data := url.Values{}
-    data.Set("version", "2")
-    data.Add("body", t.In.Body)
+	if (t.In.Compiler > 8080) || (t.In.Compiler < 8000) {
+		print("ERR1x")
+		return
+	}
 
+	var port = fmt.Sprintf("%d", t.In.Compiler)
 
-    url := "http://127.0.0.1:"+t.In.Compiler+"/compile?output=json"
-  //  fmt.Println("URL:>", url)
+	url := "http://127.0.0.1:" + port + "/compile?output=json"
+	//  fmt.Println("URL:>", url)
 
 	var payload = data.Encode()
 
+	client := &http.Client{}
+	r, err2 := http.NewRequest("POST", url, bytes.NewBufferString(payload)) // <-- URL-encoded payload
 
-    client := &http.Client{}
-    r, err2 := http.NewRequest("POST", url, bytes.NewBufferString(payload)) // <-- URL-encoded payload
+	if err2 != nil {
+		print("ERR2")
+		return
+	}
 
-	if (err2 != nil) {print("ERR2");return;}
+	//   r.Header.Add("Authorization", "auth_token=\"XXXXXXX\"")
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Content-Length", strconv.Itoa(len(payload)))
 
-//   r.Header.Add("Authorization", "auth_token=\"XXXXXXX\"")
-    r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-    r.Header.Add("Content-Length", strconv.Itoa(len(payload)))
+	resp, err3 := client.Do(r)
 
-    resp, err3 := client.Do(r)
-
-	if (err3 != nil) {print("ERR3:");fmt.Println(err3);return;}
-
+	if err3 != nil {
+		print("ERR3:")
+		fmt.Println(err3)
+		return
+	}
 
 	// so we close response
 	defer resp.Body.Close()
-        contents, err4 := ioutil.ReadAll(resp.Body)
-	if (err4 != nil) {print("ERR4");return;}
-
+	contents, err4 := ioutil.ReadAll(resp.Body)
+	if err4 != nil {
+		print("ERR4")
+		return
+	}
 
 	var jsonp = fmt.Sprintf("jscallback(%s);", string(contents))
-  //      fmt.Printf("%s\n", jsonp)
-
+	//      fmt.Printf("%s\n", jsonp)
 
 	// write to file
 	err5 := ioutil.WriteFile("/tmp/"+t.In.Id+".js", []byte(jsonp), 0644)
-	if (err5 != nil) {print("ERR5");return;}
+	if err5 != nil {
+		print("ERR5")
+		return
+	}
 
-	if resp.Status != "200 OK" {print("ERR6");return;}
+	if resp.Status != "200 OK" {
+		print("ERR6")
+		return
+	}
 
 	// dispatch file
-	go dispatch(t.In.Id)
-
+	go perr(dispatch(t.In.Id))
 
 }
-
-
-
 
 func main() {
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":"+gateport, nil)
 }
-
